df3dce240aadf5d1bbea87b0f4e2b2a21f0b88de
Nofre <davidprogram417@gmail.com>
2024-12-16 19:55:47 +0100

    added logger service http method in broker

--- broker-service/internal/handlers.go
+++ broker-service/internal/handlers.go
@@ -9,8 +9,9 @@ import (
 )
 
 type RequestPayload struct {
-	Action string      `json:"action"`
-	Auth   AuthPayload `json:"auth,omitempty"`
+	Action string        `json:"action"`
+	Auth   AuthPayload   `json:"auth,omitempty"`
+	Logger LoggerPayload `json:"logger,omitempty"`
 }
 
 type AuthPayload struct {
@@ -32,7 +33,7 @@ func (a *App) Broker(w http.ResponseWriter, r *http.Request) {
 	_ = a.writeJSON(w, http.StatusOK, payload)
 }
 
-func (a *App) Authenticate(w http.ResponseWriter, r *http.Request) {
+func (a *App) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	var requestPayload RequestPayload
 
 	err := a.readJSON(w, r, &requestPayload)
@@ -44,6 +45,8 @@ func (a *App) Authenticate(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "login":
 		a.Login(w, requestPayload.Auth)
+	case "log":
+		a.Logger(w, requestPayload.Logger)
 	default:
 		_ = a.errorJSON(w, errors.New("unknown action"), http.StatusBadRequest)
 	}
@@ -97,6 +100,54 @@ func (a *App) Login(w http.ResponseWriter, pl AuthPayload) {
 	payload.Error = false
 	payload.Message = "Authenticated"
 	payload.Data = jsonResp.Data
+
+	_ = a.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (a *App) Logger(w http.ResponseWriter, pl LoggerPayload) {
+	jsonData, _ := json.MarshalIndent(pl, "", "\t")
+
+	loggerServiceURL := "http://logger-service:8030/log"
+
+	request, err := http.NewRequest("POST", loggerServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		_ = a.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		_ = a.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	defer func() {
+		_ = response.Body.Close()
+	}()
+
+	if response.StatusCode != http.StatusAccepted {
+		_ = a.errorJSON(w, errors.New("error calling logger service"), http.StatusBadRequest)
+		return
+	}
+
+	var jsonResp jsonResponse
+	err = json.NewDecoder(response.Body).Decode(&jsonResp)
+	if err != nil {
+		_ = a.errorJSON(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	if jsonResp.Error {
+		_ = a.errorJSON(w, errors.New(jsonResp.Message), http.StatusInternalServerError)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged"
 
 	_ = a.writeJSON(w, http.StatusAccepted, payload)
 }
--- broker-service/internal/routes.go
+++ broker-service/internal/routes.go
@@ -23,7 +23,7 @@ func (a *App) Routes() http.Handler {
 
 	mux.Post("/", a.Broker)
 
-	mux.Post("/login", a.Authenticate)
+	mux.Post("/login", a.HandleSubmission)
 
 	return mux
 }
